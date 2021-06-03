dc8c0c0d1689c5e2bfdf74d252386c828201df03
Brandon Eads <brandon181989@hotmail.com>
2021-06-03 09:22:50 -0700

    fixed subtraction. no longer displays NaN

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,8 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
-function subtract(number1, number2) {
-  return number1 - number2;
+function subtract(number3, number4) {
+  return number3 - number4;
 }
 
 function multiply(number1, number2) {
@@ -21,5 +21,15 @@ $(document).ready(function() {
     const number2 = parseInt($("#add2").val());
     const result = add(number1, number2);
     $("#output").text(result);  
+  });
+});
+
+$(document).ready(function() {
+  $("form#sub").submit(function(event) {
+    event.preventDefault();
+    const number3 = parseInt($("#sub1").val());
+    const number4 = parseInt($("#sub2").val());
+    const result = subtract(number3, number4);
+    $("#outputsub").text(result);  
   });
 });
